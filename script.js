137525516961b47700e1769cfa1586840af507fe
sudouniverse <nabilsikder00@gmail.com>
2026-05-02 20:24:37 +0600

    new some code

--- script.js
+++
@@ -1,7 +0,0 @@
-console.log("lets run write some js code")
-async function main(){
-    let a = await fetch("http://127.0.0.1:3000/audio/")
-    let response =await a.text()
-    console.log(response)
-}
-main()
